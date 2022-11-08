ae0621765d1959dfdcd98386174e1ba858107b86
muhmdrdho <104894172+muhmdrdho@users.noreply.github.com>
2022-11-09 06:41:23 +0700

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -32,19 +32,24 @@ with st.sidebar:
 hide_st_style = """
                 <style>
                 #MainMenu {visibility: hidden;}
-                footer {visibility: hidden;}
+ 
                 header {visibility: hidden;}
                 </style>
                 """
 st.markdown(hide_st_style, unsafe_allow_html=True)
-hide = """
-                <style>
-                .css-1em0o4w {
-                               visibility: hidden;
-                            }
-                </style>
-                """
-st.markdown(hide, unsafe_allow_html=True)
+footer="
+
+        <style> your css code put here</style>
+
+        <div class='footer'>
+
+        <p>the word you want to tell<a style='display:block;text-align:center;' 
+
+        href='https://www.streamlit.io' target='_blank'>your email address put here</a></p>
+
+        </div>"
+
+st.markdown(footer, unsafe_allow_html=True)
 reduce_header_height_style = """
             <style>
                 div.block-container {padding-top:0rem;}
