0297c618364b28724ec7cff69c6e7400237c506b
muhmdrdho <104894172+muhmdrdho@users.noreply.github.com>
2022-11-09 12:07:53 +0700

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -28,7 +28,7 @@ option_data = [
    {'icon':"fa fa-question-circle",'label':"Unsure"},
    {'icon': "bi bi-hand-thumbs-down", 'label':"Disagree"},
 ]
-over_theme = {'txc_inactive': 'black','menu_background':'white','txc_active':'yellow','option_active':'red', 'align':'center'}
+over_theme = {'txc_inactive': 'black','menu_background':'rgb(246, 246, 246)','txc_active':'yellow','option_active':'red', 'align':'center'}
 font_fmt = {'font-class':'h2','font-size':'150%'}
 with st.sidebar:
     op2 = hc.option_bar(option_definition=option_data,title='Feedback Response',key='PrimaryOption',override_theme=over_theme,font_styling=font_fmt,horizontal_orientation=False)
