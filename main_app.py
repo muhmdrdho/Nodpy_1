0f048b4b1345445e30e989f6b7580a5b2754cb53
muhmdrdho <muhmdrdho@gmail.com>
2022-11-11 06:04:53 +0700

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -38,20 +38,7 @@ hide_st_style = """
                 </style>
                 """
 st.markdown(hide_st_style, unsafe_allow_html=True)
-footer="""
 
-        <style> your css code put here</style>
-
-        <div class='footer'>
-
-        <p>the word you want to tell<a style='display:block;text-align:center;' 
-
-        href='https://www.streamlit.io' target='_blank'>your email address put here</a></p>
-
-        </div>
-        """
-
-st.markdown(footer, unsafe_allow_html=True)
 reduce_header_height_style = """
             <style>
                 div.block-container {padding-top:0rem;}
