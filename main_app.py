1915f23cee84e97106f675ba44b9ba3cadbefe83
muhmdrdho <muhmdrdho@gmail.com>
2022-11-13 11:06:19 +0700

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,13 +1,67 @@
 from app.index_lib import *
 
 st.set_page_config( layout="wide", initial_sidebar_state="expanded")
-from app.layout.dashboard import *
+sidebar_setting = st.markdown(
+        """
+        <style>
+            .css-1fkbmr9 {
+                        background-color: rgb(245, 245, 245);
+                        background-attachment: fixed;
+                        flex-shrink: 0;
+                        height: calc(100vh - 2px);
+                        top: 0px;
+                        width: 17rem;
+                        z-index: 999991;
+                        margin-left: 0px;
+                        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+sidebar_expand_hide = st.markdown(
+        """
+        <style>
+            .css-1o0o1ai {
+                        visibility: hidden;
+                        }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+    #sidebar logo
+st.sidebar.image('app/assets/logo/Nodpy2.png')
+
+    #sidebar main menu
+with st.sidebar:
+    selected = option_menu("Main Menu",["Preacquisition", "Interpretation", "About"],
+                            icons=["file","compass","megaphone"],
+                            menu_icon="cast",
+                            default_index=0
+                            )
+
+hide_st_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+ 
+                header {visibility: hidden;}
+                </style>
+                """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+reduce_header_height_style = """
+            <style>
+                div.block-container {padding-top:0rem;}
+            </style>
+        """
+st.markdown(reduce_header_height_style, unsafe_allow_html=True)
+
+st.header("Preacquisition")
+st.markdown("---")
 
 
 
 from app.core.maps import *
-    #sidebar
-from app.navigation import *
+
     
 
     
