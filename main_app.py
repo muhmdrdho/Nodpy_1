51e5eca240e0b3245b9efa776b8fdc60d4de12db
muhmdrdho <muhmdrdho@gmail.com>
2022-11-13 22:41:26 +0700

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -281,7 +281,11 @@ if selected=="Interpretation":
                         
                         #Measure Control
             plugins.MeasureControl(position='topright', primary_length_unit='meters', secondary_length_unit='miles', primary_area_unit='sqmeters', secondary_area_unit='acres').add_to(int_map)
-            
+            coordinate_data = pd.read_csv(upload)
+            coordinate_data = coordinate_data.dropna(subset=['Latitude'])
+            coordinate_data = coordinate_data.dropna(subset=['Longitude'])
+            for i in range(len(coordinate_data)):
+                folium.Marker(location=[coordinate_data.iloc[i]['Latitude'], coordinate_data.iloc[i]['Longitude']]).add_to(sample_map)
             st_folium(int_map, width=700)
         
         
