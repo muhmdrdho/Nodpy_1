1933d3147f3a5b274ac44cc8b33c4a49f2e18a49
muhmdrdho <muhmdrdho@gmail.com>
2022-11-14 15:35:06 +0700

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -70,7 +70,9 @@ geojson = gpd.read_file(state_geo)
 geojson_states = list(geojson.SYMBOLS.values)
 final_df = geojson.merge(df_map, on="SYMBOLS")
 map_dict = df_map1.set_index('SYMBOLS')['IDX_FORMATION'].to_dict()
-color_scale = StepColormap(df_map3["CLR_IDX"], vmin = min(map_dict.values()), vmax = max(map_dict.values()))
+color_scale = LinearColormap(['darkblue','brown','blue','green','skyblue','purple','pink','cadetblue',
+                            'turquoise','blue','orange','yellow','seagreen','red','maroon','midnightblue',
+                            'aquamarine','azure','navy','teal','beige','darkgreen',], vmin = min(map_dict.values()), vmax = max(map_dict.values()))
 def get_color(feature):
     value = map_dict.get(feature['properties']['SYMBOLS'])
     if value is None:
