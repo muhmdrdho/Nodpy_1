1c7b7725005b625c7455e8932461b1df0b32882d
muhmdrdho <muhmdrdho@gmail.com>
2022-12-03 16:37:52 +0700

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -209,7 +209,7 @@ if selected=="Interpretation":
     for i in range(number_of_tabs):
         with tabs[i]:
             st.subheader("Data View")
-			cols6 = st.columns(2)
+			
             db = st.checkbox(f"Database {i+1}")
             if db:
                 choose = st.selectbox(f"See whats in our database {i+1}", dwrite["names"])
